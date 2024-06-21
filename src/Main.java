b1864ac7952274af64bf691c9d4db57f8bcf7717
UJINNNEU <y.yamov@mail.ru>
2024-06-21 23:36:23 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Main {
 public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+         // Specify the value of s (Указываем значение s)
+        int s=3; 
 
-        int s=3; // Указываем значение s
-
-        // Читаем числа как строку
-        System.out.print("Введите числo: ");
+        //Reading numbers as a string (Читаем числа как строку)
+        System.out.print("Enter a number: ");
         String digit = scanner.nextLine();
 
        if(digit.chars().allMatch( Character::isDigit))
@@ -16,13 +17,14 @@ public static void main(String[] args) {
        }
        else
        {
-           System.out.println("Неправильная запись числа!");
+           System.out.println("Incorrect number entry!");
        }
 
 
 }
 
-// Метод для печати одного числа в стиле ЖК-дисплея
+
+    //A method for printing a single number in the style of an LCD display
     public static void logicProgram(int s, String digit)
     {
 
@@ -43,7 +45,7 @@ public static void main(String[] args) {
     public static void Cycle(int s, String digit, int level)
     {
         for(int j=0;j <digit.length();j++){
-            char digit2 = digit.charAt(j);// Обрабатываем каждое число
+            char digit2 = digit.charAt(j);// We process each number (Обрабатываем каждое число)
             printNumber (digit2, s,level);
             System.out.print("  ");
         }
@@ -126,7 +128,6 @@ public static void printNumber(char digit, int s, int level) {
                     System.out.print(" "+"-".repeat(s)+" ");
                     break;
             }
-
             break;
         case '4':
             switch (level){
@@ -146,7 +147,6 @@ public static void printNumber(char digit, int s, int level) {
                     System.out.print(" "+" ".repeat(s)+" ");
                     break;
             }
-
             break;
         case '5':
             switch (level){
@@ -185,7 +185,6 @@ public static void printNumber(char digit, int s, int level) {
                     System.out.print( " "+"-".repeat(s)+" ");
                     break;
             }
-
             break;
         case '7':
             switch (level){
@@ -205,7 +204,6 @@ public static void printNumber(char digit, int s, int level) {
                     System.out.print(" " +" ".repeat(s)+" ");
                     break;
             }
-
             break;
         case '8':
             switch (level){
@@ -225,7 +223,6 @@ public static void printNumber(char digit, int s, int level) {
                     System.out.print(" "+ "-".repeat(s)+ " ");
                     break;
             }
-
             break;
         case '9':
             switch (level){
@@ -247,11 +244,7 @@ public static void printNumber(char digit, int s, int level) {
             }
 
             break;
-
-
-        default:
-           // System.out.println("Неверная цифра!");
-            ;
+        default:;
     }
 }
 }
